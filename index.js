c9fdd35753c0ba1e06643bf864a6fe3ee00a8871
pyavchik <pyavchik@gmail.com>
2018-12-01 12:56:35 +0200

    starting a server added

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer(function (req, res) {
+   res.end('Hello  World\n');
+});
+
+server.listen(3000, function () {
+   console.log("The server is listening on port 3000 now");
+});
+
