52c1b26555fc4a424b9071363ff38f529a002e0d
pyavchik <pyavchik@gmail.com>
2018-12-01 13:16:33 +0200

    added parsing request paths

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer(function (req, res) {
+
+    const parsedUrl = url.parse(req.url, true);
+
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+
    res.end('Hello  World\n');
+
+   console.log('Request received on path: '+trimmedPath);
 });
 
 server.listen(3000, function () {
