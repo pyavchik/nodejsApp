0585ddfae7194ac2378de94a42cb1bfa6514f107
pyavchik <pyavchik@gmail.com>
2018-12-08 22:29:19 +0200

    added connecting to an API example

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ var fs = require('fs');
 var handlers = require('./lib/handlers');
 var helpers = require('./lib/helpers');
 
+// @TODO GET RID OF THIS
+helpers.sendTwilioSms('639977874','Hello',function (err) {
+    console.log('this was the error', err);
+});
+
+
 // Instantiate the HTTP server
 var httpServer = http.createServer(function (req, res) {
     unifiedServer(req, res);
